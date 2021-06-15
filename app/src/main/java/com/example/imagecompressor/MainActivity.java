03e6ba306646fe331a6191147d9247b34209feff
Abdullah272056 <abdullah272056@gmail.com>
2021-06-15 21:42:19 +0600

    add dexter library and manifest permission

--- app/src/main/java/com/example/imagecompressor/MainActivity.java
+++ app/src/main/java/com/example/imagecompressor/MainActivity.java
@@ -3,8 +3,18 @@ package com.example.imagecompressor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+   
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
